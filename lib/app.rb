b19c77db24b8de13c36c1c1e27f53eac7d97a8ed
Gadiza Zerari <gadizaz@hotmail.com>
2018-04-03 18:22:05 +0100

    Made form with submi button

--- lib/app.rb
+++ lib/app.rb
@@ -13,7 +13,13 @@ get '/secret3' do
   'Secret3'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = %w[Amigo Oscar Viking].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
